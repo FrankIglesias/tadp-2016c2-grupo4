c1f0b611c0ff5656e62cb62521852a2352bda375
Nicolas <nzarewsky@gmail.com>
2016-09-06 22:17:27 -0300

    Update

--- src/tadspec.rb
+++ src/tadspec.rb
@@ -3,12 +3,22 @@ class TADsPec
   attr_accessor :test_instance
 
   def self.search_instance
-    instance_methods.grep(/testear_que_/)
+    @test_instance = instance_methods.grep(/testear_que_/)
   end
 
-module Deberia
-  
-end
+  def Ser < Tipo
+
+  end
+
+    ############# zona peligrosa
+  class Class
+    def deberia(self.tipo,bloque)
+      tipo.ejecutar(bloque)
+    end
+
+    ser = Tipo.new
+
+  end
 
 
 
