5a0434c4f12e15553b71ac64315d782fc678cd8b
Nicolas <nzarewsky@gmail.com>
2016-09-09 02:33:07 -0300

    Agrego un par de cosas para atacarlo de otra manera, tengo un drama que es deberia mayor/menor me devuelve false y no se porq

--- src/tadspec.rb
+++ src/tadspec.rb
@@ -1,48 +1,56 @@
   class TADsPec
     attr_accessor :test_instance
 
+    def initialize
+      @test_instance = []
+    end
+
     def self.search_instance
       @test_instance = instance_methods.grep(/testear_que_/)
     end
 
+    def self.testear
+      search_instance
+      @test_instance.each do |unit_test|
+        TestContex.new.correr_simple(unit_test)
+      end
+    end
+
   end
 
+  class TestContex
+     def correr_simple(block)
+       block = method_to_block(block)
+       self.instance_eval(&block)
+     end
+
+    def method_to_block(block)
+      proc{self.instance_eval(method(block.to_sym))}
+    end
+  end
 
     ############# zona peligrosa
   class Object
+
     def deberia(bloque)
-      self.instance_eval(&bloque)
-    end
-
-  end
-
-  class Object
-
-    def mayor_a(algo)
-      proc do
-        self > algo
-      end
-    end
-
-    def menor_a(algo)
-      proc do
-        self < algo
-      end
-    end
-
-    def ser(bloque, algo)
-      proc do
-        self.instance_eval(&bloque)
-      end
+      self.instance_exec(&bloque)
     end
 
     def ser(algo)
-      proc do
-        self.eql? algo
+      if block_given?
+        yield
+      else
+        proc{ self.eql? algo }
       end
     end
-
   end
-  
 
+
+    def mayor_a algo
+      proc{self > algo}
+    end
+
+    def menor_a algo
+      proc{self < algo}
+    end
 
