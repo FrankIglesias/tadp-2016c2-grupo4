786e04745e3e2ffd30dd69778233b609831e81be
Nicolas <nzarewsky@gmail.com>
2016-09-16 19:36:11 -0300

    Ahora solo corre en las instancias del test correspondiente, osea, debo definir el test para que me corra.... faltan varias cosas aun

--- src/tadspec.rb
+++ src/tadspec.rb
@@ -1,35 +1,81 @@
   class TADsPec
 
+    attr_accessor :unitTest
+
     def self.search_instance
-      @@test_instances= (Object.constants.select{|x| eval(x.to_s).is_a? Class}).map {|x| eval(x.to_s)}
-      @@test_instances= @@test_instances.select{|y| (y.instance_methods false).any? {|metodo| metodo.to_s.start_with? "testear_que"}}
+
+      @unitTest = (Object.constants).map {|c| Object.const_get(c)}
+      @unitTest = @unitTest.select {|k| k.is_a? Class}
+      @unitTest = @unitTest.select {|c| (c.instance_methods false).any? {|m| m.to_s.start_with?('testear_que_')}}
     end
 
-    ## un getter mas copado necesitariamos
-  def self.test_instances
-    @@test_instances
-  end
-
     def self.testear (*args)
+
+      Object.send :include, TestSuite
+      Proc.send :include, TestSuite
+
+      search_instance
+
+      ############# zona peligrosa
+
+      Object.send :define_method ,:deberia do |algo|
+        algo.run(self)
+      end
+
+      Proc.send :define_method ,:deberia do |algo|
+        algo.run(self)
+      end
+
+      ############ fin de zona peligrosa
+      # no me gusta esto, estamos repitiendo codigo
       if args.length>0
         args.each do
         |unit_test|
-          TestContex.new.correr_simple(unit_test)
+          unit_test.send :include, TestSuite
+          TestContex.correr(unit_test)
         end
+        remover_modulo_test
+        remover_metodos_peligrosos
       else
-        search_instance
-        @@test_instances.each do |unit_test|
-        TestContex.new.correr_simple(unit_test)
+        @unitTest.each do |unit_test|
+          unit_test.send :include, TestSuite
+          TestContex.correr(unit_test)
         end
+        #remover_modulo_test
+        remover_metodos_peligrosos
       end
     end
+
+    def self.remover_metodos_peligrosos
+      Object.send :remove_method, (:deberia)
+      Proc.send :remove_method, (:deberia)
+    end
+
+    def self.remover_modulo_test
+     TestSuite.instance_methods.each{|m| undef_method(m)}
+    end
+
   end
 
   class TestContex
-     def correr_simple(block)
-       block = method_to_block(block)
-       self.instance_eval(&block)
-     end
+    attr_accessor :object, :met
+
+    def self.correr(clase)
+      @object = clase
+      @met = (@object.instance_methods false).select {|m| m.to_s.start_with?('testear_que_')}
+      correr_simple
+    end
+
+    def self.correr_simple
+
+      @met.each do |m|
+        var = @object.class_eval{
+          test = self.new
+          puts "El resultado del test fue: #{test.send m.to_sym}"
+        }
+      end
+
+    end
 
     def method_to_block(block)
       proc{self.instance_eval(method(block.to_sym))}
@@ -110,16 +156,20 @@
 
     def method_missing(symbol,*args)
       if symbol.to_s.start_with? "ser_"
+
         TADPBlock.new (proc {|x|
         @string = symbol.to_s
         @string[0..3]= ''
         x.send(@string.to_sym) })
+
       else if symbol.to_s.start_with? "tener_"
+
              TADPBlock.new (proc {|x|
              @string = symbol.to_s
              @string[0..5]=''
              @string= ('@' + @string)
-             x.instance_variable_get(@string.to_s) == args[0]})
+             x.instance_variable_get(@string) == args[0]})
+
            else
               super(symbol, *args)
            end
@@ -128,26 +178,10 @@
 
   end
 
-    ############# zona peligrosa
-  class Object
-    include TestSuite
-    def deberia algo
-    algo.run(self)
-    end
-  end
 
-  class Proc
-    include TestSuite
-    def deberia algo
-      algo.run(self)
-    end
-  end
-
-  ############ fin de zona peligrosa
-
-    # esta clase es de prueba_se va a borrar
   class A
     attr_accessor :hello,:o
+
     def initialize
       @hello = 25
       @o="holas"
@@ -156,7 +190,16 @@
     def hola?
       true
     end
+
+    def testear_que_holis
+      a = A.new
+      a.deberia ser_hola?
+    end
+
   end
+
+
+
 
 
 =begin
