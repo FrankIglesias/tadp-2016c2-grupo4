8dcacbe93aeab1daae1c6156ac0abf845cf8c1ec
Nicolas <nzarewsky@gmail.com>
2016-09-23 02:20:53 -0300

    Test de mockeo

--- rspec/tadp_test_spec.rb
+++ rspec/tadp_test_spec.rb
@@ -109,6 +109,53 @@ describe 'TADSPEC tests' do
 
 
     expect(TADsPec.testear Persona, :testear_que_pipipi).to eq([true])
+
   end
 
+  it 'testear mock' do
+
+    class PersonaHome
+      def todas_las_personas
+        # Este método consume un servicio web que consulta una base de datos
+      end
+      def personas_viejas
+        self.todas_las_personas.select{|p| p.viejo?}
+      end
+    end
+
+    class Persona
+      attr_accessor :edad
+      def initialize algo
+        edad = algo
+      end
+
+      def viejo?
+        self.edad > 29
+      end
+    end
+    
+    class PersonaHomeTests
+
+      def testear_que_personas_viejas_trae_solo_a_los_viejos
+
+        nico = Persona.new(30)
+        axel = Persona.new(30)
+        lean = Persona.new(22)
+
+        # Mockeo el mensaje para no consumir el servicio y simplificar el test
+
+        PersonaHome.mockear(:todas_las_personas) do
+          [nico, axel, lean]
+        end
+
+        viejos = PersonaHome.new.personas_viejas
+        viejos.deberia ser [nico, axel]
+
+      end
+
+    end
+
+    expect(TADsPec.testear PersonaHomeTests).to eq([true])
+    
+  end
 end
