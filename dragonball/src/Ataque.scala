c04982389bc282dd18828ec892ad7504de102536
FrankIglesias <frank.beatle@gmail.com>
2016-11-17 02:00:26 -0300

    ultimos cambios

--- dragonball/src/Ataque.scala
+++ dragonball/src/Ataque.scala
@@ -9,14 +9,17 @@ class Ataque {
   
 }
 
-class AtaqueDeEnergia(duelo: Duelo, ataque: (Duelo => (Integer, Integer))) extends Ataque {
+class AtacarConEnergia(duelo: Duelo, ataque: AtaqueDeEnergia) extends Ataque {
    def apply(duelo:Duelo) = {
      defensor(duelo).especie match {
        case Androide(_) => cambiarKiYGenerarDuelo(ataque(duelo))
-       case _ => cambiarKiYGenerarDuelo(-ataque(duelo))
+       case _ => cambiarKiYGenerarDuelo(negarSegundoParametro(ataque(duelo)))
      }
+     
    }
-   
+   def negarSegundoParametro(delta:(Integer,Integer)){
+     (delta._1,-delta._2)
+   }
    def cambiarKiYGenerarDuelo(delta : (Integer, Integer)) {
      generarDueloNuevo(atacante(duelo).copy(atacante(duelo).ki - delta._1 min atacante(duelo).kiMaximo))(defensor(duelo).copy(ki = defensor(duelo).ki + delta._2 min defensor(duelo).kiMaximo))
    }
@@ -28,8 +31,10 @@ class AtaqueFisico {
 
 trait Ataques {
   
-  def KameHameHa(duelo:Duelo => Integer) {
-     return juntarKi
+  case class Onda(cantidadDeKi:Integer) {
+    def apply(duelo:Duelo => (Integer,Integer)){
+     (cantidadDeKi,cantidadDeKi); 
+    }
   }
   
 }
--- dragonball/src/Tipos.scala
+++ dragonball/src/Tipos.scala
@@ -5,7 +5,7 @@ object Tipos {
   type Duelo = (Guerrero,Guerrero)
   type Movimiento = Duelo => Duelo
   type Digerir = Guerrero => Guerrero
-  
+  type AtaqueDeEnegia = Duelo => (Integer, Integer)
   def atacante(duelo:Duelo) : Guerrero = duelo._1
   def defensor(duelo:Duelo) : Guerrero = duelo._2
   
