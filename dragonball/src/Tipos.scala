5eee9265a0414f7535aaeec354890b30e984a2a0
FrankIglesias <frank.beatle@gmail.com>
2016-11-17 02:40:53 -0300

    genero criterio

--- dragonball/src/Tipos.scala
+++ dragonball/src/Tipos.scala
@@ -8,6 +8,6 @@ object Tipos {
   type AtaqueDeEnegia = Duelo => (Integer, Integer)
   def atacante(duelo:Duelo) : Guerrero = duelo._1
   def defensor(duelo:Duelo) : Guerrero = duelo._2
-  
+  type Criterio =Duelo => Integer
   def generarDueloNuevo(atacante:Guerrero)(defensor:Guerrero): Duelo=(atacante.copy(),defensor.copy())
 }
