52b94efb3c74366c60735a7eafe3e5c81649e7b4
FrankIglesias <frank.beatle@gmail.com>
2016-11-18 11:14:52 -0300

    QUE NO ROMPA

--- dragonball/src/Criterio.scala
+++ dragonball/src/Criterio.scala
@@ -11,16 +11,24 @@ object Criterios {
     class movimientoMasDanino extends Criterio
     {
       
-      def min(t1: (Int, Int), t2: (Int, Int)): (Int, Int) = if (t1._1 < t2._2) t1 else t2
+      def max(t1: (Int, Movimiento), t2: (Int, Movimiento)): (Int, Movimiento) = if (t1._1 > t2._2) t1 else t2
+      def aplicarDuelo(mov: Movimiento) : (Duelo, Movimiento) = 
+      {
+        return (mov(duelo), mov)
+      }
+      
+      def sacarCoef(t1: (duelo :Duelo, mov: Movimiento)): (Int,  Movimiento) = 
+      {
+        return (defensor(duelo).ki/defensorInicial.ki, mov)
+      }
       
       def apply(listaMovimientos: List[Movimiento], duelo : Duelo) : Movimiento = {
         val atacanteInicial = atacante(duelo)
         val defensorInicial = defensor(duelo)
-        var i : Integer = 0
         
-        var movMasEfectivo = listaMovimientos.map(mov => mov(duelo)).map(duelito => (defensor(duelito).ki/defensorInicial.ki, i + 1)).reduceLeft(min)
+        var movMasEfectivo = listaMovimientos.map(mov => aplicarDuelo(mov)).map((duelo, mov) => sacarCoef((duelo, mov))).reduceLeft(max)
         
-        return listaMovimientos(movMasEfectivo._2 - 1)
+        return listaMovimientos(movMasEfectivo._2)
       }
     }
   
